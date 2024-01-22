94b8120cb880d1e7e54e3db474d2add26fb8a068
Avi Kothari <avikothari84@gmail.com>
2024-01-22 18:12:02 +0530

    TEMP: Add new function

--- telecom_library/__init__.py
+++ telecom_library/__init__.py
@@ -1,18 +1,18 @@
 from telecom_library.main import (
-    # get_site_inventory_for_site_id,
+    get_site_inventory_for_site_id,
     fetch_assigned_incidents,
     analyze_incident,
-    # get_slack_channel_id_for_site_manager,
+    get_slack_channel_id_for_site_manager,
     # send_slack_message,
     file_report,
     close_ticket
 )
 
 __all__ = [
-    # "get_site_inventory_for_site_id",
+    "get_site_inventory_for_site_id",
     "fetch_assigned_incidents",
     "analyze_incident",
-    # "get_slack_channel_id_for_site_manager",
+    "get_slack_channel_id_for_site_manager",
     # "send_slack_message",
     "file_report",
     "close_ticket"
